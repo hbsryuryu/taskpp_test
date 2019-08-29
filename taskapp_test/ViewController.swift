aad87be276a54a6e8f6d4f0738ba8f09750b417d
hbsryuryu <hbsryuryu@gmail.com>
2019-08-29 14:31:58 +0900

    Initial Commit

---
+++ taskapp_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp_test
+//
+//  Created by 濱田龍輝 on 2019/08/29.
+//  Copyright © 2019 Ryuuki.hamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
